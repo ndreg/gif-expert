4952c360e045162b3f5835b45b253a4468700a1f
Andres <fullbringdres@gmail.com>
2021-04-21 19:19:47 -0500

    03. Se empieza a trabajar con useState.

--- src/Components/GifExpertApp.jsx
+++ src/Components/GifExpertApp.jsx
@@ -1,8 +1,26 @@
-const GifExpert = () => (
+import {useState} from 'react';
+
+const GifExpert = () => {
+
+  
+  const [categories, setCategories] = useState(['Jujutsu Kaisen', 'One Piece', 'Cats']);
+
+  const handleAdd = () => {
+    setCategories(cats => [...cats, 'HunterxHunter']);
+  }
+
+  return (
   <>
   <h2>Gif Expert App</h2>
   <hr/>
+  <ul>
+  {
+    categories.map(element => <li key={element}>{element}</li>)
+  }
+  </ul>
+  <button onClick={()=> handleAdd()}>Add a new category</button>
   </>
-);
+  )
+};
 
 export default GifExpert;
