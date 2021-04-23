619f791c0f780a20d16b9eae98c1a59a0bcb36ae
Andres <fullbringdres@gmail.com>
2021-04-23 04:35:41 -0500

    10. Se agregan animaciones con animate.css

--- src/Components/GifGridItem.jsx
+++ src/Components/GifGridItem.jsx
@@ -1,7 +1,7 @@
 export const GifGridItem = ({title, gif}) => {
   
   return (
-    <div className="gif-card">
+    <div className="gif-card animate__animated animate__fadeIn">
       <img src={gif} alt={title}/>
       <p>{title}</p>
     </div>
